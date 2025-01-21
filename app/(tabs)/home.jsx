de3e8ba625c5f4cf1c1a57533d89f8757f701a89
sarachafikidrissi <chafikidrissisara@gmail.com>
2025-01-21 14:51:29 +0100

    create data and fetch it from app write

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,31 +1,42 @@
-import { View, Text, FlatList, Image, RefreshControl } from "react-native";
-import React from "react";
+import { View, Text, FlatList, Image, RefreshControl, Alert } from "react-native";
+import React, { useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
 import SearchInput from "../../components/SearchInput";
 import Trending from "../../components/Trending";
 import EmptyState from "../../components/EmptyState";
 import { useState } from "react";
+import { getAllPosts } from "../../lib/appwrite";
+import useAppwrite from "../../lib/useAppwrite";
 
 const Home = () => {
   const [refreshing, setRefreshing] = useState(false);
+
+  //* calling customized hook to fetch posts data
+  const {data: posts, refetch} = useAppwrite
+  (getAllPosts());
+
 
   const onRefresh = async () => {
     setRefreshing(true);
 
     // re call videos -> if new videos appeared
+    await refetch();
 
     setRefreshing(false);
   };
+console.log(posts);
+
+  
   return (
     <SafeAreaView className="bg-primary h-full">
       //* FlatList is designed for rendering dynamic lists based on data.
       <FlatList
-        data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+        data={posts}
         // data={[]}
         keyExtractor={(item) => item.$id}
         renderItem={({ item }) => (
-          <Text className="text-3xl text-white">{item.id}</Text>
+          <Text className="text-3xl text-white">{item.title}</Text>
         )}
         ListHeaderComponent={() => (
           <View className="my-6 px-4 space-y-6 ">
