d2a493eff18e8a63c73a4079e8c6c37ce64ffd3a
sarachafikidrissi <chafikidrissisara@gmail.com>
2025-02-11 12:58:03 +0100

    allow user to choose videos or images from his phone to upload them to database

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -4,14 +4,18 @@ import {
   SafeAreaView,
   ScrollView,
   TouchableOpacity,
+  Alert,
 } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import tw from "twrnc";
 
 import FormField from "../../components/FormField";
 import { ResizeMode, Video } from "expo-av";
 import { icons } from "../../constants";
 import { Image } from "react-native";
 import CustomButton from "../../components/CustomButton";
+import * as DocumentPicker from "expo-document-picker";
+import { router } from "expo-router";
 
 const Create = () => {
   //* to know if we are currently uploading a video
@@ -23,12 +27,62 @@ const Create = () => {
     thumbnail: "",
     prompt: "",
   });
+  /**
+   * a function that helps you pick images and videos from phone
+   */
+  const openPicker = async (selectType) => {
+    const result = await DocumentPicker.getDocumentAsync({
+      type:
+        selectType === "image"
+          ? ["image/png", "image/jpg"]
+          : ["video/mp4", "video/gif"],
+    });
+
+    if (!result.canceled) {
+      if (selectType === "video") {
+        setForm({ ...form, video: result.assets[0].uri });
+      }
+      if (selectType === "image") {
+        setForm({ ...form, thumbnail: result.assets[0].uri });
+      }
+    } else {
+      setTimeout(() => {
+        Alert.alert("Document picked", JSON.stringify(result, null, 2));
+      }, 100);
+    }
+  };
+
+  const submitt = () => {
+    if (!form.title || !form.video || !form.thumbnail) {
+      Alert.alert("Please fill in all the fields");
+    }
+
+    setUploading(true);
+    try {
+      
+
+      Alert.alert('Success', 'Post Uploaded Successfully')
+      router.push('/home')
+
+    } catch (error) {
+      Alert.alert('Error', error.message)
+    } finally {
+      setForm({
+        title: "",
+        video: null,
+        thumbnail: "",
+        prompt: "",
+      });
+    }
+  };
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView className="px-4 my-6 ">
         <Text className="text-white text-2xl font-psemibold ">
           Upload Video
         </Text>
+        {/* Title Area */}
         <FormField
           title="Video Title"
           value={form.title}
@@ -41,20 +95,22 @@ const Create = () => {
           }
           otherStyles="mt-10"
         />
-        <View className="mt-7 space-y-2">
+        {/* Area to click on and upload video */}
+        <View className="mt-7 gap-y-2">
           <Text className="text-base text-gray-100 font-pmedium">
             Upload Video
           </Text>
-          {/* Area to click on and upload video */}
-          <TouchableOpacity>
+          <TouchableOpacity onPress={() => openPicker("video")}>
             {form.video ? (
-              <Video
-                source={{ uri: form.video.uri }}
-                className="w-full h-64 rounded-2xl "
-                useNativeControls
-                resizeMode={ResizeMode.COVER}
-                isLooping
-              />
+              <View>
+                <Video
+                  source={{ uri: form.video }}
+                  style={tw`w-full h-64 rounded-2xl`}
+                  useNativeControls
+                  resizeMode={ResizeMode.COVER}
+                  isLooping
+                />
+              </View>
             ) : (
               <View className="w-full h-40 px-4 bg-black-100 rounded-2xl justify-center items-center">
                 <View className="w-14 h-14 border border-dashed border-secondary-100 justify-center items-center">
@@ -68,17 +124,17 @@ const Create = () => {
             )}
           </TouchableOpacity>
         </View>
-        {/* for the upload video thumbnail */}
-        <View className="mt-7 space-y-2 ">
+        {/* upload video thumbnail  Area*/}
+        <View className="mt-7 gap-y-2 ">
           <Text className="text-base text-gray-100 font-pmedium">
             Thumbnail Image
           </Text>
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={() => openPicker("image")}>
             {form.thumbnail ? (
               <Image
-                source={{ uri: form.thumbnail.uri }}
-                resizeMethod="cover"
+                source={{ uri: form.thumbnail }}
+                resizeMode="cover"
                 className="h-64 w-full rounded-2xl"
               />
             ) : (
@@ -95,7 +151,7 @@ const Create = () => {
             )}
           </TouchableOpacity>
         </View>
-      {/* Ai propmt Area */}
+        {/* Ai propmt Area */}
         <FormField
           title="AI Prompt"
           value={form.prompt}
@@ -109,7 +165,11 @@ const Create = () => {
           otherStyles="mt-7"
         />
         {/* render custom button */}
-        <CustomButton containerStyles={'mt-7'} title='Sunmit & Publish' isLoading={uploading}/>
+        <CustomButton
+          containerStyles={"mt-7"}
+          title="Sunmit & Publish"
+          isLoading={uploading}
+        />
       </ScrollView>
     </SafeAreaView>
   );
